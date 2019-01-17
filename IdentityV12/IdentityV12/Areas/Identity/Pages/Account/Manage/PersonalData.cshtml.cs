b4bd36d5110f5e99e671e6dbf0d14f61c21c438b
Osku Sirpoma <osku.sirpoma@gmail.com>
2019-01-17 13:32:13 +0200

    Password reseting tool done

---
+++ IdentityV12/IdentityV12/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using IdentityV12.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityV12.Areas.Identity.Pages.Account.Manage
+{
+    public class PersonalDataModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<PersonalDataModel> _logger;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public PersonalDataModel(
+            UserManager<ApplicationUser> userManager,
+            ILogger<PersonalDataModel> logger,
+             RoleManager<ApplicationRole> roleManager,
+             SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
+        }
+
+        public async Task DeactivateAccount()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var roleResult = await _userManager.RemoveFromRoleAsync(user, "Admin");
+            await _signInManager.SignOutAsync();
+            return ;
+        }
+    }
+}
